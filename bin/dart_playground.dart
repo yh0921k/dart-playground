2bf0c03b4bb72337ca3a38186d249686791acff4
Yonghwi Kim <yh0921k@gmail.com>
2023-03-05 17:00:00 +0900

    feat: final, const

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,14 +1,17 @@
 void main(List<String> arguments) {
-  // nullable - null일 수 있음
-  // non-nullable - null이 될 수 없음
+  final String name = "Dart";
+  // name = "Flutter"; // final 변수는 선언한 이후 값을 변경하지 못함
 
-  // String name = null; // Error
-  String? name = 'Dart';
-  name = null;
-  print(name);
+  const int age = 10;
+  // age = 20; // const 변수는 선언한 이후 값을 변경하지 못함
 
-  String nonNullName = 'Dart';
-  print(nonNullName!); // 현재 이 값이 null일 경우 에러
+  // final, const는 타입 생략 가능
+  final flutter = "Flutter";
+  const hour = 20;
+
+  final DateTime now = DateTime.now();
+  // const DateTime now = DateTime.now(); // Error: const는 빌드 시간에 값을 알고 있어야 하며, DateTime.now()는 런타임에 해당 값을 알 수 있음
+  print(now);
 }
 
 void printLine(bool addEmptyLine) {
