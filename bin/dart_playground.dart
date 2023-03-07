926ff9322b0580333d5c8470702a19812bd8bbfb
Yonghwi Kim <yh0921k@gmail.com>
2023-03-07 17:00:00 +0900

    feat: Map

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,30 +1,38 @@
 void main(List<String> arguments) {
-  // List
-  // List<String> name = ["A", "B", "C", 4]; // Error
-  List<String> names = ["A", "B", "C"];
-  List<int> numbers = [1, 2, 3, 4, 5];
-  print(names);
-  print(numbers);
-  
+  // Map
+  Map<String, String> dictionary = {
+    "Dart" : "다트",
+    "Flutter" : "플러터",
+    "Java" : "자바"
+  };
+  print(dictionary);
   printLine(true);
 
-  // index
-  print(names[0]);
-  // print(names[4]); // Error
-
-  // length
-  print(names.length);
-
   // add
-  names.add("D");
-  print(names);
+  dictionary["Typescript"] = "타입스크립트";
+
+  // addAll
+  Map<String, String> another = {
+    'Javascript' : "자바스크립트"
+  };
+  dictionary.addAll(another);
+  print(dictionary);
+
+  // get by key
+  print(dictionary["Dart"]);
+
+  // modify
+  dictionary['Dart'] = '다트(Dart)';
+  print(dictionary);
+  printLine(true);
 
   // remove
-  names.remove("C");
-  print(names);
+  dictionary.remove("Dart");
+  print(dictionary);
 
-  // indexOf
-  print(names.indexOf("B"));
+  // get keys, values
+  print(dictionary.keys);
+  print(dictionary.values);
 }
 
 void printLine(bool addEmptyLine) {
