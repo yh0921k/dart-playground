0387d2cb208340d3c37a6e32d67d1a988bc4a3f8
Yonghwi Kim <yh0921k@gmail.com>
2023-03-08 17:00:00 +0900

    feat: typedef

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,38 +1,28 @@
 void main(List<String> arguments) {
-  int result = addNumbers([1, 2, 3, 4, 5]);
-  print('result : $result');
+  Operation operation = add;
+  print(operation(1, 2, 3));
 
-  result = optionalAddNumbers(10, 15);
+  operation = subtract;
+  print(subtract(10, 9, 1));
+  printLine(true);
+
+  // 사용예시
+  int result = calculate(30, 40, 50, add);
   print(result);
-
-  result = namedAddNumber(z: 15, y: 10);
-  print(result);
-
-  // 기본 positional parameter와 named parameter는 조합해서 사용 가능(코드 생략
-  // arrow function
-  int sum = arrowAddNumbers(20, y: 10);
-  print(sum);
 }
 
-int addNumbers(List<int> numbers) {
-  return numbers.reduce((value, element) => value += element);
-}
+typedef Operation = int Function(int x, int y, int z);
 
-// optional parameter
-int optionalAddNumbers(int x, [int y = 20, int z = 30]) {
-  int sum = x + y + z;
-  return sum;
-}
+// add
+int add(int x, int y, int z) => x + y + z;
 
-// named parameter : 순서가 중요하지 않음
-// required 키워드를 제공하면 optional parameter로 사용 가능
-int namedAddNumber({int x = 10, required int y, required int z}) {
-  int sum = x + y + z;
-  return sum;
-}
+// subtract
+int subtract(int x, int y, int z) => x - y - z;
 
-// arrow function
-int arrowAddNumbers(int x, { required int y, int z = 20}) => x + y + z;
+// calculate
+int calculate(int x, int y, int z, Operation operation) {
+  return operation(x, y, z);
+}
 
 void printLine(bool addEmptyLine) {
   if (addEmptyLine) {
