a5a4d865b5fdca62e6fcb471ee4d47c3820dd0a3
Yonghwi Kim <yh0921k@gmail.com>
2023-03-08 17:00:00 +0900

    feat: 열거형

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,59 +1,9 @@
 void main(List<String> arguments) {
-  // loop
-  for(int i = 0; i <= 10; i++) {
-    print(i);
-  }
-
-  // sum
-  int total = 0;
-  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  for(int i = 0; i < numbers.length; i++) {
-    total += numbers[i];
-  }
-  print(total);
-  printLine(true);
-
-  // for in
-  total = 0;
-  for(int number in numbers) {
-    print(number);
-    total += number;
-  }
-  print(total);
-  printLine(true);
-
-  // while
-  total = 0;
-  while(total < 10) {
-    total++;
-  }
-  print(total);
-
-  // do while
-  total = 0;
-  do {
-    total++;
-  } while(total < 10);
-  print(total);
-  printLine(true);
-
-  // break
-  total = 0;
-  while(total < 10) {
-    total += 1;
-    if(total == 5) {
-      break;
-    }
-  }
-  print(total);
-  printLine(true);
-
-  // continue
-  for(int i = 0; i < 10; i++) {
-    if(i == 5) {
-      continue;
-    }
-    print(i);
+  // enum
+  Status status = Status.approved;
+  print(status);
+  if(status == Status.approved) {
+    print("승인");
   }
 }
 
@@ -63,4 +13,8 @@ void printLine(bool addEmptyLine) {
   }
 
   print("=" * 20);
+}
+
+enum Status {
+  approved, pending, rejected
 }
