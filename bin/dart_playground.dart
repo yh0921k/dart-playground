07aa1e84dcaa91f0a388b52e3c9509269a70c557
Yonghwi Kim <yh0921k@gmail.com>
2023-03-08 17:00:00 +0900

    feat: 함수

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,11 +1,38 @@
 void main(List<String> arguments) {
-  // enum
-  Status status = Status.approved;
-  print(status);
-  if(status == Status.approved) {
-    print("승인");
-  }
+  int result = addNumbers([1, 2, 3, 4, 5]);
+  print('result : $result');
+
+  result = optionalAddNumbers(10, 15);
+  print(result);
+
+  result = namedAddNumber(z: 15, y: 10);
+  print(result);
+
+  // 기본 positional parameter와 named parameter는 조합해서 사용 가능(코드 생략
+  // arrow function
+  int sum = arrowAddNumbers(20, y: 10);
+  print(sum);
 }
+
+int addNumbers(List<int> numbers) {
+  return numbers.reduce((value, element) => value += element);
+}
+
+// optional parameter
+int optionalAddNumbers(int x, [int y = 20, int z = 30]) {
+  int sum = x + y + z;
+  return sum;
+}
+
+// named parameter : 순서가 중요하지 않음
+// required 키워드를 제공하면 optional parameter로 사용 가능
+int namedAddNumber({int x = 10, required int y, required int z}) {
+  int sum = x + y + z;
+  return sum;
+}
+
+// arrow function
+int arrowAddNumbers(int x, { required int y, int z = 20}) => x + y + z;
 
 void printLine(bool addEmptyLine) {
   if (addEmptyLine) {
@@ -13,8 +40,4 @@ void printLine(bool addEmptyLine) {
   }
 
   print("=" * 20);
-}
-
-enum Status {
-  approved, pending, rejected
 }
