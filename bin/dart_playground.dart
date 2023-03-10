7db3a61a11472f85d930700d4c726f179e5938f4
Yonghwi Kim <yh0921k@gmail.com>
2023-03-10 17:00:00 +0900

    feat: generic

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,34 +1,23 @@
 void main(List<String> arguments) {
-  Student student = Student('Dart');
-  student.sayName();
+  Student<String> dart = Student('A0001', 'Dart');
+  dart.printStudentNumberType();
 
-  printLine(true);
-  print(student is Student);
-  print(student is PersonInterface);
-
+  Student<int> java = Student(1000, 'Java');
+  java.printStudentNumberType();
 }
 
-// interface : abstract 키워드를 붙여 인스턴스화를 막을 수 있음
-abstract class PersonInterface {
-  String name;
+// generic - 타입을 외부에서 받을때 사용
+class Student<T> {
+  final T studentNumber;
+  final String name;
 
-  PersonInterface(this.name);
+  Student(this.studentNumber, this.name);
 
-  void sayName(); // abstract 클래스는 함수 몸체를 지울 수 있음
-}
-
-class Student implements PersonInterface {
-  @override
-  String name;
-
-  Student(this.name);
-
-  @override
-  void sayName() {
-    print("I'm $name");
+  void printStudentNumberType() {
+    print(studentNumber.runtimeType);
   }
-
 }
+
 
 void printLine(bool addEmptyLine) {
   if (addEmptyLine) {
