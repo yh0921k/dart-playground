5898dc799e2af8c83dd4228645270964b0f3a8a8
Yonghwi Kim <yh0921k@gmail.com>
2023-03-11 17:00:00 +0900

    feat: example

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,12 +1,44 @@
 void main(List<String> arguments) {
-  List<int> even = [2, 4, 6, 8, 10];
-  List<int> odd = [1, 3, 5, 7, 9];
+  final List<Map<String, String>> data = [
+    {'Name': 'Dart', 'Age': '5'},
+    {'Name': 'Java', 'Age': '30'},
+    {'Name': 'Spring', 'Age': '20'}
+  ];
+  print(data);
 
-  // cascading operator
-  final result = [...even, ...odd];
+  // 프론트엔드는 어떤 형식의 데이터를 받을지 알 수 없어 아래와 같은 구조화가 필수
+  // 실제 각각의 Map 데이터에는 name, age 속성이 있는지 모르기 때문에 '!'를 명시하여 해당 속성이 있다는 것을 강제
+  final List<Person> people = data.map((e) => Person(name: e['Name']!, age: e['Age']!)).toList();
+  print(people);
+
+  printLine(true);
+  for (Person person in people) {
+    print(person.name);
+  }
+
+  printLine(true);
+  final parsedPeople = people.where((person) => int.parse(person.age) >= 10);
+  print(parsedPeople);
+
+  // Functional Programming
+  printLine(true);
+  final result = data
+      .map((e) => Person(name: e['Name']!, age: e['Age']!))
+      .where((element) => int.parse(element.age) >= 10)
+      .fold(0, (previousValue, element) => previousValue + element.name.length);
   print(result);
-  print(even == [...even]);
+}
 
+class Person {
+  final String name;
+  final String age;
+
+  Person({required this.name, required this.age});
+
+  @override
+  String toString() {
+    return 'Person(name:$name, age:$age';
+  }
 }
 
 void printLine(bool addEmptyLine) {
