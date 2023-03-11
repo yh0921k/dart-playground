d9b7cb1f998ea6436f38ae0b492a09637908a4d8
Yonghwi Kim <yh0921k@gmail.com>
2023-03-11 17:00:00 +0900

    feat: Map의 map()

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,15 +1,12 @@
 void main(List<String> arguments) {
-  List<String> names = ['Dart', 'Java', 'Flutter', 'Spring'];
-  
-  List<String> addPrefixNames = names.map((name) => "Name : $name").toList();
-  print(addPrefixNames);
-  print(names == addPrefixNames);
-  printLine(true);
+  Map<String, String> grades = {'A': '95', 'B': '90', 'C': '85'};
+  final result = grades.map((key, value) => MapEntry(key, '점수: $value'));
+  print(result);
 
-  String numbers = '13579';
-  final parsed = numbers.split('').map((number) => '$number.jpg');
-  print(parsed);
-
+  final keys = grades.keys.toList();
+  final values = grades.values.map((value) => '정수: $value').toList();
+  print(keys);
+  print(values);
 }
 
 void printLine(bool addEmptyLine) {
