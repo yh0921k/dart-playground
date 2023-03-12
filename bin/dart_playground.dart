a000ae6f47ad7b0dcf0d2aa497e73d563346079d
Yonghwi Kim <yh0921k@gmail.com>
2023-03-12 17:00:00 +0900

    feat: Stream을 await과 비슷한 작업을 하는 방법
    - 기본적으로 dart:async 와 같은 패키지를 쓸 일은 거의 없음(더 잘 만들어진 오픈소스 및 라이브러리 사용)

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,11 +1,12 @@
 main(List<String> arguments) async {
-  calculate(1).listen((event) {
-    print('calculate(1): $event');
+  playAllStream().listen((event) {
+    print(event);
   });
+}
 
-  calculate(4).listen((event) {
-    print('calculate(4): $event');
-  });
+Stream<int> playAllStream() async* {
+  yield* calculate(1); // yield*은 해당하는 스트림의 모든 값이 리턴될 때까지 기다림(Future의 await과 비슷함)
+  yield* calculate(1000);
 }
 
 Stream<int> calculate(int number) async* {
