8211a2709ce270073dbbae7983df2d99e51968ab
Yonghwi Kim <yh0921k@gmail.com>
2023-03-12 17:00:00 +0900

    feat: Stream에서의 Future 적용
    - 기본적으로 Stream을 반환하여 listen()을 하기위해 반환타입 및 async*을 사용
    - 더하여 로직 내에서 Future를 사용할 수 있음

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,22 +1,19 @@
-import 'dart:async';
-
 main(List<String> arguments) async {
-  final controller = StreamController();
-  // final stream = controller.stream; // 기본적으로 한 번 리스닝을 할 수 있는 스트림이 생김
-  final stream = controller.stream.asBroadcastStream(); // 여러 번 리스닝을 할 수 있는 스트림 생성
-
-  final streamListener1 = stream.where((event) => event % 2 == 1).listen((event) {
-    print("Listener1 : $event");
+  calculate(1).listen((event) {
+    print('calculate(1): $event');
   });
 
-  final streamListener2 = stream.where((event) => event % 2 == 0).listen((event) {
-    print("Listener2 : $event");
+  calculate(4).listen((event) {
+    print('calculate(4): $event');
   });
-  controller.sink.add(1);
-  controller.sink.add(2);
-  controller.sink.add(3);
-  controller.sink.add(4);
-  controller.sink.add(5);
+}
+
+Stream<int> calculate(int number) async* {
+  for (int i = 0; i < 5; i++) {
+    yield i * number;
+    
+    await Future.delayed(Duration(seconds: 1));
+  }
 }
 
 void printLine(bool addEmptyLine) {
