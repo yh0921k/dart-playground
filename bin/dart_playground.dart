78b95225553ae06f4a4aa7e96471ca5707be59e5
Yonghwi Kim <yh0921k@gmail.com>
2023-03-25 17:00:00 +0900

    feat: DateTime 기본

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,26 +1,12 @@
 main(List<String> arguments) async {
-  playAllStream().listen((event) {
-    print(event);
-  });
-}
+  DateTime now = DateTime.now();
 
-Stream<int> playAllStream() async* {
-  yield* calculate(1); // yield*은 해당하는 스트림의 모든 값이 리턴될 때까지 기다림(Future의 await과 비슷함)
-  yield* calculate(1000);
-}
-
-Stream<int> calculate(int number) async* {
-  for (int i = 0; i < 5; i++) {
-    yield i * number;
-    
-    await Future.delayed(Duration(seconds: 1));
-  }
-}
-
-void printLine(bool addEmptyLine) {
-  if (addEmptyLine) {
-    print("");
-  }
-
-  print("=" * 20);
+  print(now);
+  print(now.year);
+  print(now.month);
+  print(now.day);
+  print(now.hour);
+  print(now.minute);
+  print(now.second);
+  print(now.millisecond);
 }
