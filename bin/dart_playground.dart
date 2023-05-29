d29a9d0533ce4e44086ae3ceecbf53fe40f839b1
Yonghwi Kim <yh0921k@gmail.com>
2023-05-29 17:00:00 +0900

    feat: Destructuring - List
    - 값을 분해한 상태로 받아옴
    - 타입 명시 가능

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,14 +1,8 @@
 // Destructuring: 값을 분해한 상태로 받아옴
 main(List<String> arguments) async {
-  final dart = ('Dart', 20);
-  print(dart.$1);
-  print(dart.$2);
-
-  // final name = dart.$1;
-  // final age = dart.$2
-
-  // Record
-  final (name, age) = ('Dart', 20);
-  print(name);
-  print(age);
+  // List
+  final languages = ['Dart', 'Java'];
+  final [String a, String b] = languages; // 타입 명시 가능
+  print(a);
+  print(b);
 }
