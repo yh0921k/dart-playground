c9936111a3f74bfb09c34c5824540831fc912057
Yonghwi Kim <yh0921k@gmail.com>
2023-05-30 17:00:00 +0900

    feat: Rest Matching
    - 리스트 안에서 한 번만 사용 가능

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,8 +1,9 @@
 // Destructuring: 값을 분해한 상태로 받아옴
 main(List<String> arguments) async {
-  // List
-  final languages = ['Dart', 'Java'];
-  final [String a, String b] = languages; // 타입 명시 가능
-  print(a);
-  print(b);
+  // Rest Matching
+  final numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  final [x, y, ..., z] = numbers; // Rest Matching: 리스트 안에서 한 번만 사용 가능
+  print(x);
+  print(y);
+  print(z);
 }
