1c0a5d35f3ccd5833b88c5ff021d8d77b6054c55
Yonghwi Kim <yh0921k@gmail.com>
2023-05-31 17:00:00 +0900

    feat: Rest Matching with name
    - 매핑된 Rest Matching에 이름을 부여할 수 있음

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,9 +1,7 @@
 // Destructuring: 값을 분해한 상태로 받아옴
 main(List<String> arguments) async {
-  // Rest Matching
+  // Rest Matching with name
   final numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  final [x, y, ..., z] = numbers; // Rest Matching: 리스트 안에서 한 번만 사용 가능
-  print(x);
-  print(y);
-  print(z);
+  final [xx, yy, ...items, zz] = numbers;
+  print(items);
 }
