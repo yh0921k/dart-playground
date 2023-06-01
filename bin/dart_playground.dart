781e0bd8b3124c6cb01c789f12f1bb2efc2dd22d
Yonghwi Kim <yh0921k@gmail.com>
2023-06-01 17:00:00 +0900

    feat: Destructuring - Ignore
    - 실제로 무시된 값을 가져 올 수 있는 방법은 없음

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,7 +1,8 @@
 // Destructuring: 값을 분해한 상태로 받아옴
 main(List<String> arguments) async {
-  // Rest Matching with name
+  // Ignore
   final numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  final [xx, yy, ...items, zz] = numbers;
-  print(items);
+  final [xxx, yyy, ...rest, zzz, _] = numbers; // 실제로 가져올 수 있는 방법이 없음
+  print(rest);
+  print(zzz);
 }
