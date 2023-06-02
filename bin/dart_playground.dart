79431b40a5678434a3d4fb4d3579746ef8e8e036
Yonghwi Kim <yh0921k@gmail.com>
2023-06-02 17:00:00 +0900

    feat: Destructuring - Map

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,8 +1,8 @@
 // Destructuring: 값을 분해한 상태로 받아옴
 main(List<String> arguments) async {
-  // Ignore
-  final numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  final [xxx, yyy, ...rest, zzz, _] = numbers; // 실제로 가져올 수 있는 방법이 없음
-  print(rest);
-  print(zzz);
+  // Map
+  final sampleMap = {'name': 'Dart', 'age': 20};
+  final {'name': mapKey, 'age': mapValue} = sampleMap;
+  print(mapKey);
+  print(mapValue);
 }
