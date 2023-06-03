6d6a2b07f84e0feae6fc6f86fea62325a4637c5d
Yonghwi Kim <yh0921k@gmail.com>
2023-06-03 17:00:00 +0900

    feat: Destructuring - Class

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,8 +1,15 @@
 // Destructuring: 값을 분해한 상태로 받아옴
 main(List<String> arguments) async {
-  // Map
-  final sampleMap = {'name': 'Dart', 'age': 20};
-  final {'name': mapKey, 'age': mapValue} = sampleMap;
-  print(mapKey);
-  print(mapValue);
+  // Class
+  final language = Language(name: 'Dart', age: 20);
+  final Language(name: languageName, age: languageAge) = language;
+  print(languageName);
+  print(languageAge);
+}
+
+class Language {
+  final String name;
+  final int age;
+
+  Language({required this.name, required this.age});
 }
