a356c01b15f615a0579e15dee56e82862dcc9eb6
Yonghwi Kim <yh0921k@gmail.com>
2023-06-05 17:00:00 +0900

    feat: Pattern Matching - Switch Validation 예제
    - Map, Class 등 기존 구조분홰 할당에서 사용했던 것들도 사용이 가능

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,11 +1,32 @@
 // Pattern Matching
 main(List<String> arguments) async {
-  // Validation
-  final language = ('Dart', 20);
+  // switcher('aaa');
+  // switcher('bbb');
+  // switcher(['1', '2']);
+  // switcher([1, 2]);
+  // switcher([1, 2, 3]);
+  // switcher([4, 5, 6]);
+  // switcher([4, 5, 6, 7]);
+  // switcher([4, 5]);
+  // switcher([4, '5']);
 
-  final (name as String, age as int) = language;
-  // final(name as int, age as int) = language; // 런타임 에러 발생
+  switcher(7); // switch 문에서 조건절 사용 예시
+  switcher(17); // switch 문에서 조건절 사용 예시
+}
 
-  print(name);
-  print(age);
+void switcher(dynamic anything) {
+  switch (anything) {
+    case 'aaa':
+      print('match: aaa');
+    case ['1', '2']:
+      print('match: [1, 2]');
+    case [_, _, _]:
+      print('match: [_, _, _]');
+    case [int a, int b]:
+      print('match: [int $a, int $b]'); // $를 명시하여 실제 값을 출력해볼 수 있음
+    case < 10 && > 5:
+      print('match: < 10 && > 5');
+    default:
+      print('no match');
+  }
 }
