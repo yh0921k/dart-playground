37608a19b722fc826485f4229aaf208ea2d4ca08
Yonghwi Kim <yh0921k@gmail.com>
2023-06-06 17:00:00 +0900

    feat: Switch 구문 변화

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,32 +1,13 @@
 // Pattern Matching
 main(List<String> arguments) async {
-  // switcher('aaa');
-  // switcher('bbb');
-  // switcher(['1', '2']);
-  // switcher([1, 2]);
-  // switcher([1, 2, 3]);
-  // switcher([4, 5, 6]);
-  // switcher([4, 5, 6, 7]);
-  // switcher([4, 5]);
-  // switcher([4, '5']);
-
-  switcher(7); // switch 문에서 조건절 사용 예시
-  switcher(17); // switch 문에서 조건절 사용 예시
+  print(switcher(5, true));
+  print(switcher(7, true));
+  print(switcher(7, false));
 }
 
-void switcher(dynamic anything) {
-  switch (anything) {
-    case 'aaa':
-      print('match: aaa');
-    case ['1', '2']:
-      print('match: [1, 2]');
-    case [_, _, _]:
-      print('match: [_, _, _]');
-    case [int a, int b]:
-      print('match: [int $a, int $b]'); // $를 명시하여 실제 값을 출력해볼 수 있음
-    case < 10 && > 5:
-      print('match: < 10 && > 5');
-    default:
-      print('no match');
-  }
-}
+// Switch 문을 반환
+String switcher(dynamic value, bool condition) => switch (value) {
+      5 => 'match: 5',
+      7 when condition => 'match 7 and true',
+      _ => 'no match',
+    };
