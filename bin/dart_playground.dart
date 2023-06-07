844390bf8414a592ccaad34f54ad87b512c3ddf3
Yonghwi Kim <yh0921k@gmail.com>
2023-06-07 17:00:00 +0900

    feat: For Loop

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,13 +1,23 @@
 // Pattern Matching
 main(List<String> arguments) async {
-  print(switcher(5, true));
-  print(switcher(7, true));
-  print(switcher(7, false));
+  forLooper();
 }
 
-// Switch 문을 반환
-String switcher(dynamic value, bool condition) => switch (value) {
-      5 => 'match: 5',
-      7 when condition => 'match 7 and true',
-      _ => 'no match',
-    };
+void forLooper() {
+  final List<Map<String, dynamic>> languages = [
+    {'name': 'Dart', 'age': 20},
+    {'name': 'Flutter', 'age': 30}
+  ];
+
+  for (final language in languages) {
+    print(language['name']);
+    print(language['age']);
+  }
+
+  print('=' * 30);
+
+  for (var {'name': name, 'age': age} in languages) {
+    print(name);
+    print(age);
+  }
+}
