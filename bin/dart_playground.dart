0856f981da67445d4645401a8fbd88d7141e7b59
Yonghwi Kim <yh0921k@gmail.com>
2023-06-10 17:00:00 +0900

    feat: Class - base

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,9 +1,11 @@
-main(List<String> arguments) async {}
+main(List<String> arguments) async {
+}
 
-// final로 클래스 선언
-// - extends, implements, mixin으로 사용이 불가능
-// - 같은 파일 안에서는 가능
-final class Person {
+// base로 클래스 선언
+// - extends는 가능하지만 implements는 불가능
+// - OOP 상에서 상속만 가능하게 할 수 있음(다트에는 인터페이스 타입이 없어 기존에 파악이 불편했음)
+// - base, sealed, final로 선언된 클래스만 extends가 가능
+base class Person {
   final String name;
   final int age;
 
