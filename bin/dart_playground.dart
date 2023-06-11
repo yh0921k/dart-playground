4f7649aef9a75601163bb7590da4f1a06e537d6c
Yonghwi Kim <yh0921k@gmail.com>
2023-06-11 17:00:00 +0900

    feat: Class - sealed
    - 타입이 추가되는 경우 에러 발생
    - switch 키워드에서 에러가 발생하며, 모든 타입에 대해 현재 로직으로 판단할 수 없음을 의미
    - 나머지 패턴에 대해 무시하는 로직을 추가하거나 Developer 타입을 처리하는 케이스를 추가해야함

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,8 +1,17 @@
-// interface로 클래스 선언
-// - implements만 가능
-interface class Person {
-  final String name;
-  final int age;
+main(List<String> arguments) async {}
 
-  Person({required this.name, required this.age});
-}
+// sealed로 클래스 선언
+// - abstract 이면서 final인 클래스
+// - 패턴 매칭을 사용할 수 있도록 해줌
+sealed class Person {}
+
+class Student extends Person {}
+
+class Engineer extends Person {}
+
+class Developer extends Person {}
+
+String whoIsHe(Person person) => switch (person) {
+      Student s => '학생',
+      Engineer e => '엔지니어',
+    };
