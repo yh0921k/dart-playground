aa7958298125faa34f0447ea5c89df29a457d2bd
Yonghwi Kim <yh0921k@gmail.com>
2023-06-11 17:00:00 +0900

    feat: Class - interface

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,11 +1,6 @@
-main(List<String> arguments) async {
-}
-
-// base로 클래스 선언
-// - extends는 가능하지만 implements는 불가능
-// - OOP 상에서 상속만 가능하게 할 수 있음(다트에는 인터페이스 타입이 없어 기존에 파악이 불편했음)
-// - base, sealed, final로 선언된 클래스만 extends가 가능
-base class Person {
+// interface로 클래스 선언
+// - implements만 가능
+interface class Person {
   final String name;
   final int age;
 
